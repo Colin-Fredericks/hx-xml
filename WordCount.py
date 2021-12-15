8995d02d895ec77eec98aafc58d3564c0b9768c0
Colin-Fredericks <1929508+Colin-Fredericks@users.noreply.github.com>
2021-12-15 15:21:14 -0500

    Simple word counter for SRT and markdown

---
+++ WordCount.py
@@ -0,0 +1,127 @@
+import os
+import re
+import sys
+import argparse
+from glob import glob
+
+instructions = """
+To use:
+python3 word_count.py filename (options)
+
+Gets the word count from SRT files, YouTube transcripts,
+and Markdown files. Not guaranteed to perfectly match any
+other word counter, but should be within 10%.
+
+Valid options:
+  -h Help. Print this message.
+  -o Output filename.
+
+Last update: Dec 15th 2021
+"""
+
+##############################################
+# Get the word count from the file
+##############################################
+def countWords(f):
+    word_count = 0
+    for line in f:
+
+        # Skip blank lines.
+        if len(line) == 0 or line == "\n" or line == "\r":
+            continue
+        # Check for SRT time lines and skip them.
+        if re.search("\d\d --> \d\d:", line):
+            continue
+        # Skip lines that are just a single number.
+        if re.search("^\d+$", line):
+            continue
+        # Check for lines that are just times and skip them.
+        if re.search("^\d\d:\d\d$", line):
+            continue
+        if re.search("^\d\d:\d\d:\d\d$", line):
+            continue
+
+        # Don't include HTML tags
+        # TODO: Not sure how to handle that one at the moment...
+        # ...especially since they might be split over multiple lines.
+        # Might need to bring in the big guns.
+
+        # TODO: Handle LaTeX? Might be easier to split than HTML, really.
+
+        raw_words = line.split(" ")
+        reduced_words = []
+        for w in raw_words:
+            # Don't include 1-character "words"
+            if len(w) > 1:
+                reduced_words.append(w)
+
+        # Store filename and count
+        word_count += len(reduced_words)
+
+    return word_count
+
+
+##############################################
+# Walk through the files.
+##############################################
+def walkFiles(file_names):
+    results = []
+    # Open all the files.
+    for name in file_names:
+        # Make sure every file exists.
+        if not os.path.exists(name):
+            print("File not found: " + name)
+            continue
+
+        # We don't currently handle folders.
+        if os.path.isfile(name):
+            with open(name, "r") as f:
+                results.append({"name": name, "word_count": countWords(f)})
+        else:
+            sys.exit("Skipping directory: " + name)
+            continue
+
+    return results
+
+
+##############################################
+# Main starts here
+##############################################
+def MakeNewRun(argv):
+
+    # Read in the filename and options
+    parser = argparse.ArgumentParser(usage=instructions, add_help=False)
+    parser.add_argument("source_files", default=None, nargs="*")
+    parser.add_argument("-h", "--help", action="store_true")
+    parser.add_argument("-o", default="word_count.csv")
+
+    args = parser.parse_args()
+    if args.help or args.source_files is None:
+        sys.exit(instructions)
+
+    # Replace arguments with wildcards with their expansion.
+    # If a string does not contain a wildcard, glob will return it as is.
+    file_names = list()
+    for f in args.source_files:
+        file_names += glob(f)
+
+    # If the filenames don't exist, say so and quit.
+    if file_names == []:
+        sys.exit("No file or directory found by that name.")
+
+    results = walkFiles(file_names)
+    results_flat = ""
+
+    # Print the totals to screen.
+    for r in results:
+        print(r["name"] + ": " + str(r["word_count"]))
+        results_flat += r["name"] + "," + str(r["word_count"]) + "\n"
+
+    # Put them in a file.
+    new_file = open(args.o, "w")
+    new_file.write(results_flat)
+    new_file.close()
+
+
+if __name__ == "__main__":
+    MakeNewRun(sys.argv)
